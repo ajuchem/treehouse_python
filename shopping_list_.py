bf25c70fcc6ef21d3b46c0ec8a6b28f2a2e4470e
ajuchem <ajuchem@daudt.com>
2017-12-02 17:57:08 -0700

    add clear_screen() to polish app

--- shopping_list_.py
+++ shopping_list_.py
@@ -1,7 +1,13 @@
+import os
+
 # make a list to hold onto our items
 shopping_list = []
 
+def clear_screen():
+	os.system("cls" if os.name == "nt" else "clear")
+
 def show_help():
+	clear_screen()
     # print out instructions on how to use the app
     print("What should we pick up at the store?")
     print("""
@@ -11,6 +17,7 @@ Enter 'SHOW' to see your current list.
 """)
 
 def show_list():
+	clear_screen()
     # print out the list
     print("Here is your list:")
     for item in shopping_list:
@@ -20,6 +27,7 @@ def add_to_list(new_item):
     # add new items to list
     shopping_list.append(new_item)
     print("Added! List now has {} items.".format(len(shopping_list)))
+    show_list()
 
 show_help()
 
